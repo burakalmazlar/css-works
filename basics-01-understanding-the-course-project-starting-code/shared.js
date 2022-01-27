99cc0ed5377747167bf959368b17565ded89d5ed
burak almazlar <burak.almazlar@epias.com.tr>
2022-01-27 17:30:47 +0300

    Section 9: Working with JavaScript & CSS

---
+++ basics-01-understanding-the-course-project-starting-code/shared.js
@@ -0,0 +1,39 @@
+const backdrop = document.querySelector(".backdrop");
+const modal = document.querySelector(".modal");
+const modalActionYes = document.querySelector(".modal a.modal__action");
+const modalActionNo = document.querySelector(".modal button.modal__action");
+
+const toggleButton = document.querySelector(".toggle-button");
+const mobileNav = document.querySelector(".mobile-nav");
+
+const close = (e) => {
+  e.preventDefault();
+  // backdrop.style.display = '';
+  // modal.style.display = '';
+  // mobileNav.style.display = '';
+  backdrop.classList.remove("open");
+  modal.classList.remove("open");
+  mobileNav.classList.remove("open");
+};
+
+backdrop.addEventListener("click", close);
+modalActionYes.addEventListener("click", close);
+modalActionNo.addEventListener("click", close);
+
+const selectPlanButton = document.querySelectorAll(".plan .button");
+
+selectPlanButton.forEach((btn) => {
+  if (btn) {
+    btn.addEventListener("click", () => {
+      backdrop.classList.add("open");
+      modal.classList.add("open");
+      // backdrop.style.display = 'initial';
+      // modal.style.display = 'initial';
+    });
+  }
+});
+
+toggleButton.addEventListener("click", () => {
+  backdrop.classList.add("open");
+  mobileNav.classList.add("open");
+});
